9b7dd3e6377f91f67090ece7ff2fb452923f9514
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2023-08-05 22:17:25 +0100

    Promo Code Done

---
+++ app/Models/PromoCode.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PromoCode extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'promo_code',
+        'scope',
+        'percentage_off',
+        'status'
+    ];
+}
---
+++ database/migrations/2023_08_05_192220_create_promo_codes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('promo_codes', function (Blueprint $table) {
+            $table->id();
+            $table->string('promo_code')->unique();
+            $table->string('scope')->default('all');
+            $table->double('percentage_off');
+            $table->integer('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('promo_codes');
+    }
+};
