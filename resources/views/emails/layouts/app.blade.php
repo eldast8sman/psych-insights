ef14ab5dea495819c564eed811246b69d9a4f194
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2023-10-02 11:23:36 +0100

    Email Verified

---
+++ resources/views/emails/layouts/app.blade.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>HTML Email</title>
+    <style type="text/css">
+      body {
+        margin: 0;
+        background-color: #cccccc;
+      }
+      table {
+        border-spacing: 0;
+      }
+      td {
+        padding: 0;
+      }
+      img {
+        border: 0;
+      }
+
+      .wrapper {
+        width: 100%;
+        table-layout: fixed;
+        background-color: #cccccc;
+        padding-bottom: 40px;
+      }
+      .main {
+        background-color: #ffffff;
+        margin: 0 auto;
+        width: 100%;
+        max-width: 600px;
+        border-spacing: 0;
+        font-family: sans-serif;
+        color: #4a4a4a;
+      }
+      @media screen and (max-width: 600px) {
+      }
+    </style>
+  </head>
+  <body>
+    <center class="wrapper">
+      <table class="main" width="100%" style="padding: 30px">
+        <tr>
+          <td>
+            <table width="100%">
+              <tr>
+                <td>
+                  <div
+                    style="
+                      display: flex;
+                      align-items: center;
+                      justify-content: space-around;
+                    "
+                  >
+                    <a
+                      href="#"
+                      style="
+                        display: flex;
+                        align-items: center;
+                        gap: 5px;
+                        text-decoration: none;
+                        color: #2e2f32;
+                      "
+                      title="PsychInsights"
+                    >
+                      <img
+                        src="https://psychinsight-email-icons.s3.us-east-2.amazonaws.com/logo.svg"
+                        alt="Psych-logo"
+                      />
+                      <h2>PsychInsights</h2>
+                    </a>
+                    <img
+                      src="https://psychinsight-email-icons.s3.us-east-2.amazonaws.com/line.svg"
+                      alt="line"
+                    />
+                  </div>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+
+        @yield('content')
+
+        <tr>
+          <td>
+            <table width="90%" style="margin: auto">
+              <tr>
+                <td
+                  style="
+                    background-color: #f0f1f9;
+                    border-radius: 10px;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                  "
+                >
+                  <a href="#">
+                    <img
+                      src="https://psychinsight-email-icons.s3.us-east-2.amazonaws.com/Facebook.svg"
+                      alt="Facebook"
+                    />
+                  </a>
+                  <a href="#" style="margin: 0px 15px">
+                    <img
+                    src="https://psychinsight-email-icons.s3.us-east-2.amazonaws.com/twitter.svg"
+                    alt=/twitter.svg" />
+                  </a>
+                  <a href="#">
+                    <img
+                      src="https://psychinsight-email-icons.s3.us-east-2.amazonaws.com/Facebook.svg"
+                      alt="Facebook"
+                    />
+                  </a>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </center>
+  </body>
+</html>
