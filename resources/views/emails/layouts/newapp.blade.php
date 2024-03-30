6b586bce067a7250464a0c8306a120ecac407929
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2024-03-30 21:55:33 +0100

    daily quote

---
+++ resources/views/emails/layouts/newapp.blade.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>HTML Email</title>
+    <style type="text/css">
+      body {
+        margin: 0;
+        background-color: #cccccc;
+      }
+      table {
+        border-spacing: 0;
+      }
+      td {
+        padding: 0;
+      }
+      img {
+        border: 0;
+      }
+
+      .wrapper {
+        width: 100%;
+        table-layout: fixed;
+        background-color: #cccccc;
+        padding-bottom: 40px;
+      }
+      .main {
+        background-color: #ffffff;
+        margin: 0 auto;
+        width: 100%;
+        max-width: 600px;
+        border-spacing: 0;
+        font-family: sans-serif;
+        color: #4a4a4a;
+      }
+      @media screen and (max-width: 600px) {
+      }
+    </style>
+  </head>
+
+  <body>
+    <center class="wrapper">
+      <table class="main" width="100%" style="padding: 30px">
+        <tr>
+          <td>
+            <table width="100%">
+              <tr>
+                <td>
+                  <div
+                    style="
+                      display: flex;
+                      align-items: center;
+                      justify-content: space-around;
+                    "
+                  >
+                    <a
+                      href="https://psychinsightsapp.com"
+                      style="
+                        display: flex;
+                        align-items: center;
+                        gap: 5px;
+                        text-decoration: none;
+                        color: #2e2f32;
+                      "
+                      title="PsychInsights"
+                    >
+                      <img
+                        src="https://psychinsight-email-icons.s3.us-east-2.amazonaws.com/logo.svg"
+                        alt="Psych-logo"
+                      />
+                      <h2>PsychInsights</h2>
+                    </a>
+                    <img
+                      src="https://psychinsight-email-icons.s3.us-east-2.amazonaws.com/line.svg"
+                      alt="line"
+                    />
+                  </div>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            <table width="100%">
+              <tr>
+                <td>
+                  <div
+                    style="
+                      display: flex;
+                      text-align: center;
+                      align-items: center;
+                      gap: 10px;
+                      justify-content: center;
+                      color: #000000;
+                    "
+                  >
+                    <p
+                      style="
+                        font-size: 40px;
+                        font-weight: 500;
+                        text-align: center;
+                      "
+                    >
+                      Verify your email
+                    </p>
+
+                    <img
+                      src="https://psychinsight-email-icons.s3.us-east-2.amazonaws.com/checked.svg"
+                      alt="checked-icon"
+                      width="50px"
+                    />
+                  </div>
+                  <p
+                    style="
+                      text-align: center;
+                      color: #000000;
+                      margin-top: -20px;
+                      font-size: 14px;
+                    "
+                  >
+                    Please verify email to secure your account
+                  </p>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            <table width="100%">
+              <tr>
+                <td style="font-size: 14px">
+                  <h1 style="text-align: center; color: #000000">
+                    You’re almost there! <br />
+                    Just confirm your email
+                  </h1>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            <table width="100%">
+              <tr>
+                <td style="font-size: 14px">
+                  <p style="margin: 40px 0px">Hello [User's First name],</p>
+                  <p style="margin-top: -20px">
+                    You’ve successfully created a Psych Insights account. To
+                    activate your account please enter this code.
+                  </p>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            <table width="100%">
+              <tr>
+                <td style="text-align: center">
+                  <p>Your code:</p>
+                  <span
+                    style="
+                      margin-top: -10px;
+                      color: #ffffff;
+                      padding: 15px 55px;
+                      background: #207384;
+                      border-radius: 10px;
+                      text-decoration: none;
+                      letter-spacing: 10px;
+                      font-size: 25px;
+                      font-weight: bold;
+                    "
+                  >
+                    123455
+                  </span>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            <table width="90%" style="margin: auto">
+              <tr>
+                <td
+                  style="
+                    display: flex;
+                    align-items: center;
+                    justify-content: space-between;
+                    font-size: 14px;
+                    margin-top: 30px;
+                  "
+                >
+                  <p>
+                    Not you? Contact
+                    <a href="" style="color: #207384">Support</a>
+                  </p>
+                  <p>
+                    Want to know more about terms of use?<a
+                      href=""
+                      style="color: #207384"
+                      >Click here</a
+                    >
+                  </p>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            <table width="90%" style="margin: auto">
+              <tr>
+                <td
+                  style="
+                    background-color: #f0f1f9;
+                    border-radius: 10px;
+                    padding: 5px 0px;
+                    display: flex;
+                    gap: 10px;
+                    align-items: center;
+                    justify-content: center;
+                  "
+                >
+                  <a
+                    href="https://www.tiktok.com/@_psychinsights_?_t=8igxYC9rlr8&_r=1"
+                    class="tiktolk"
+                    >TikTok</a
+                  >
+                  <a
+                    href="https://www.instagram.com/_psych_insights_?igsh=aXd1bHB0eWJvYTBy"
+                    class="instagram"
+                    >Instagram</a
+                  >
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </center>
+  </body>
+</html>
