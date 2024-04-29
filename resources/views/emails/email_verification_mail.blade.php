226c3d971c92d2d211e41e4c2798c50bb307426f
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2024-04-29 04:44:38 +0100

    Emails

--- resources/views/emails/email_verification_mail.blade.php
+++ resources/views/emails/email_verification_mail.blade.php
@@ -1,28 +1,52 @@
 @extends('emails.layouts.newapp')
 
-@section('title')
-  <p
-  style="
-    font-size: 40px;
-    font-weight: 500;
-    text-align: center;
-  "
-  >
-  Verify your email
-  </p>
-
-  <img
-    src="https://psychinsight-email-icons.s3.us-east-2.amazonaws.com/white-instagram.png"
-    alt="checked-icon"
-    width="50px"
-  />
-@endsection
-
-@section('subtitle')
-  Please verify Email to secure Account
-@endsection
-
 @section('content')
+<tr>
+  <td>
+    <table width="100%">
+      <tr>
+        <td>
+          <div
+            style="
+              display: flex;
+              text-align: center;
+              align-items: center;
+              gap: 10px;
+              justify-content: center;
+              color: #000000;
+            "
+          >
+            <p
+              style="
+                font-size: 40px;
+                font-weight: 500;
+                text-align: center;
+              "
+            >
+              Verify your email
+            </p>
+            <img
+              src="https://psychinsight-email-icons.s3.us-east-2.amazonaws.com/checked.png"
+              alt="padlock-icon"
+              width="50px"
+            />
+          </div>
+          <p
+            style="
+              text-align: center;
+              color: #000000;
+              margin-top: -20px;
+              font-size: 14px;
+            "
+          >
+            Please verify Email to secure Account
+          </p>
+        </td>
+      </tr>
+    </table>
+  </td>
+</tr>
+
 <tr>
   <td>
     <table width="100%">
