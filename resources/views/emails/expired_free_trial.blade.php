eef6d053974421d085ae89c4f5e6ddef8065575e
Omotolani Olurotimi <omotolaniolurotimi@gmail.com>
2024-12-29 03:30:35 +0100

    Mail

--- resources/views/emails/expired_free_trial.blade.php
+++ resources/views/emails/expired_free_trial.blade.php
@@ -44,6 +44,7 @@
           <p style="margin-bottom: 20px; text-align: left">
             Thank you for trying out the 7-day free trial of <strong>Psych Insights Premium!</strong> We hope the enhanced features added value to your mental well-being journey.
             <br />
+            <br />
             As of today, your account has been automatically transitioned to our <strong>Basic (Free)</strong> plan.
             You'll still have access to many essential tools to support your mental health. But if you're missing the added benefits of Premium,
             upgradig is quick and easy. Simply click the <strong>"Upgrade Now"</strong> button at the top of your home page to unlock Premium features again.
--- resources/views/emails/subscription_successful_mail.blade.php
+++ resources/views/emails/subscription_successful_mail.blade.php
@@ -44,11 +44,15 @@
           <p style="margin-bottom: 20px; text-align: left">
             Welcome to the <strong>Psych Insights Premium</strong> community!  We are absolutely thrilled to have you on board.
             <br />
+            <br />
             With your premium subscription, you've unlocked a suite of exclusive features and tools designed to empower your mental health journey and provide deeper insights into your well-being
+            <br />
             <br />
             We're here to support you every step of the way. If you have any questions or need assistance with your Premium features, don't hesitate to reach out to us at <i>support@psychinsightsapp.com</i>
             <br />
+            <br />
             Thank you for choosing <strong>Psych Insights</strong> - It's an honour to be part of your journey toward greated mental wekk-being
+            <br />
             <br />
             Warm regards,
             <br />
